8697736dae011c8720f81995647ffefbe3520860
safwen-code <safwendjebbi1234@gmail.com>
2025-05-30 21:39:49 +0100

    add navigation to comp

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Search from './components/Search'
 
-export default function App() {
-  return <Search />
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import FilmDetails from './components/FilmDetails'
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Search} />
+        <Stack.Screen
+          name="Detailfilm"
+          component={FilmDetails}
+          options={{ title: 'Film Details' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
+export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
