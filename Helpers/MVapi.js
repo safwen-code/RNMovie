3b1b94cb852a19c2cc132478c33220eec516fffd
safwen-code <safwendjebbi1234@gmail.com>
2025-05-30 23:25:55 +0100

    create fct to get film by id

--- Helpers/MVapi.js
+++ Helpers/MVapi.js
@@ -1,12 +1,23 @@
 const api = 'ca7772cab3600a541ec81c6af24f6645'
 
 //get film by text
-export const getfilmbytext = (text) => {
+export const getfilmbytext = (text, page) => {
+  // console.log(text)
   const url =
-    'https://api.themoviedb.org/3/search/movie?api_key=' +
+    'https://api.themoviedb.org/3/search/movie?query=' +
+    text +
+    '&api_key=' +
     api +
-    '&language=fr&query=' +
-    text
+    '&page=' +
+    page
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+//get film by id
+export const getfilmbyid = (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api}&language=fr`
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
