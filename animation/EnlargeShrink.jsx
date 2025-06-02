70a9f2ee99641ff87d98798309bb041db48f66c8
safwen-code <safwendjebbi1234@gmail.com>
2025-06-02 15:41:19 +0100

    update Anim comp

--- animation/EnlargeShrink.jsx
+++ animation/EnlargeShrink.jsx
@@ -2,15 +2,11 @@ import React, { useEffect, useRef } from 'react'
 import { Animated } from 'react-native'
 
 const EnlargeShrink = ({ shouldEnlarge, children }) => {
-  const viewSize = useRef(new Animated.Value(getSize(shouldEnlarge))).current
-
-  const getSize = (shouldEnlarge) => {
-    return shouldEnlarge ? 80 : 40
-  }
+  const viewSize = useRef(new Animated.Value(shouldEnlarge ? 80 : 40)).current
 
   useEffect(() => {
     Animated.spring(viewSize, {
-      toValue: getSize(shouldEnlarge),
+      toValue: shouldEnlarge ? 80 : 40,
       useNativeDriver: false,
     }).start()
   }, [shouldEnlarge])
